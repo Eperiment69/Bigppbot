82b1e4a3c74fd4c85c36b01bf6182e7f63b5a00e
mackdroid <68280146+mackdroid@users.noreply.github.com>
2021-10-08 23:07:39 +0530

    add 2 cmds

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ import random
 from replit import db
 import requests
 import json
+from links import hugs, pissedoff, waifuu, topicsfw, topicnsfw
 # import interactions
-description = " Macky, smol pp "
+description = "A bot, with big pp"
 intents = discord.Intents.default()
 intents.members = True
 client = commands.Bot(command_prefix='$', description=description, intents=intents)
@@ -56,7 +57,7 @@ async def inspireusryan(ctx):
 @client.command()
 async def howgayami(ctx):
   gayperc = random.randint(1,100)
-  await ctx.send("You are " + str(gayperc) + '% gay')
+  await ctx.send("You are " + str(gayperc) + '% gay :rainbow_flag:')
 
 @client.command()
 async def toss(ctx):
@@ -83,18 +84,32 @@ async def ppsize(ctx):
     
 @client.command()
 async def hug(ctx):
-  hug = ['https://media0.giphy.com/media/l8ooOxhcItowwLPuZn/giphy.gif?cid=ecf05e47mi97mmlb52g2bxt31izuo825cinr2tpge26yi17b&rid=giphy.gif&ct=g', 'https://media2.giphy.com/media/4No2q4ROPXO7T6NWhS/giphy.gif?cid=ecf05e47mi97mmlb52g2bxt31izuo825cinr2tpge26yi17b&rid=giphy.gif&ct=g', 'https://media4.giphy.com/media/U4LhzzpfTP7NZ4UlmH/giphy.gif?cid=ecf05e47mi97mmlb52g2bxt31izuo825cinr2tpge26yi17b&rid=giphy.gif&ct=g', 'https://media2.giphy.com/media/ZBQhoZC0nqknSviPqT/giphy.gif?cid=ecf05e47mi97mmlb52g2bxt31izuo825cinr2tpge26yi17b&rid=giphy.gif&ct=g', 'https://media3.giphy.com/media/chyZacnjg8q0XMp8my/giphy.gif?cid=ecf05e47i55xjwhawhfoq4yhezhf4s7zk6ihm4eddzleznir&rid=giphy.gif&ct=g', 'https://media2.giphy.com/media/TiDgr2Zas5gZdoFJxR/giphy.gif?cid=ecf05e47gcvn45933e63ca95foapsx7z5h6m3301fclmdclf&rid=giphy.gif&ct=g', 'https://media2.giphy.com/media/NhjPhBQIIxdxm/giphy.gif?cid=790b7611ce653ccce81b60c01505dcb1ca4870bcf4dee63a&rid=giphy.gif&ct=g'] 
+  hug = hugs
   await ctx.send(random.choice(hug))
 
 @client.command()
 async def pissoff(ctx):
-  pissoff = ['https://media0.giphy.com/media/4WF4YilkoC7sjzG3TY/giphy.gif?cid=ecf05e47ay6qhakebn5ymu5s8cbkqyyixnivnmr1e6nrlyig&rid=giphy.gif&ct=g', 'https://media4.giphy.com/media/13sY5MBe3khsly/giphy.gif?cid=ecf05e47iq1eiwuo34nihcswb9pzoemf7af0lyufvmney3ec&rid=giphy.gif&ct=g', 'https://tenor.com/view/i-think-i-found-something-look-adam-dimarco-randall-carpio-the-order-i-discovered-something-gif-18043211', 'https://media1.giphy.com/media/uYOgd0l1uRVbq/giphy.gif?cid=ecf05e47f57wwzqcthwlgdlo1uhep9tilfanq4jfwrk01phf&rid=giphy.gif&ct=g', 'https://media0.giphy.com/media/QGzPdYCcBbbZm/giphy.gif?cid=ecf05e47pp88oj6knidhs9velp74dxbfb4d9kn26503rv6lf&rid=giphy.gif&ct=g', 'https://media0.giphy.com/media/x1kS7NRIcIigU/giphy.gifps://www.google.com/search?q=traps&iflsig=ALs-wAMAAAAAYV3VMJ_xVAI7i6FbVVjma0Ghw9f8XBuG?cid=ecf05e47oxfa5uvqzkwrsntc8fhomjzduagfsbv6r6v9kekm&rid=giphy.gif&ct=g', 'https://media2.giphy.com/media/XHr6LfW6SmFa0/giphy.gif?cid=ecf05e47pz9a4ania3bphryyxqcvf3jzm5j87bg08o8qu6wf&rid=giphy.gif&ct=g']
+  pissoff = pissedoff
   await ctx.send(random.choice(pissoff))  
   
 @client.command()
 async def waifu(ctx):
-  waifu = ['https://media3.giphy.com/media/sMeN0FzG7lsw8/giphy.gif?cid=ecf05e478m1b1bcp0ti6hd2xqqefq9jv0n2tzdu5jzspzkay&rid=giphy.gif&ct=g', 'https://media4.giphy.com/media/LML5ldpTKLPelFtBfY/giphy.gif?cid=ecf05e47jkynm5zvpscrlh47iafp2m25rbd80ubglsf87uz2&rid=giphy.gif&ct=g', 'https://media0.giphy.com/media/10YWqUivkQPeeJWD3u/giphy.gif?cid=ecf05e47brubnww16fff9x8azlzbuukr4lf5pj6rztuimlut&rid=giphy.gif&ct=g', 'https://media0.giphy.com/media/du1u1eYSXp0wo/giphy.gif?cid=ecf05e47wo9un68r4kgy8vxk25k2bytsdkv78xvdibmhyuxq&rid=giphy.gif&ct=g', 'https://media0.giphy.com/media/cxPtMDHG8Ljry/giphy.gif?cid=ecf05e47zntncg3nfwg7hx0lyc2ct9m2mlcxpf0fomyw4rfh&rid=giphy.gif&ct=g', 'https://media3.giphy.com/media/hZfBs4E6tCTc19njbD/giphy.gif?cid=ecf05e47xuk5a7xqrr8mu1qwiandty9jdvkeaq0arkjawp00&rid=giphy.gif&ct=g', 'https://media3.giphy.com/media/5os6xBZJEobuE3Wnmj/giphy.gif?cid=ecf05e47ive8dr3uye3o5vfhriv1iktn5m28tfrtiqcz4dvx&rid=giphy.gif&ct=g', 'https://media4.giphy.com/media/xF2i5FhIo3Igeuzt5t/giphy.gif?cid=ecf05e47x1j3rai599c59tvxm0tssydrafauzslg2iga48hc&rid=giphy.gif&ct=g', 'https://media3.giphy.com/media/gSGNyzkKeKFBS/giphy.gif?cid=ecf05e47pmvjqow3efzmr1owpm7i049kuo9b72xyl31lyv8c&rid=giphy.gif&ct=g', 'https://media1.giphy.com/media/Qcrmc6dbGyLMQ/giphy.gif?cid=ecf05e47l15fd653nxhpz1r5emiabrdk30p8qoxbqtsjix0z&rid=giphy.gif&ct=g']
+  waifu = waifuu
   await ctx.send(random.choice(waifu))
+  
+@client.command()
+async def nudes(ctx):
+  link = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+  #gif = 'http://dozydozy.tumblr.com/post/3750667564'
+  await ctx.send(link)
+
+@client.command()
+async def cat(ctx):
+  link = 'https://api.thecatapi.com/v1/images/search'
+  response = requests.get(link)
+  response_json = json.loads(response.text)
+  link = response_json[0]['url']
+  await ctx.send(link)
 
 @client.command()
 async def dog(ctx):
@@ -109,4 +124,21 @@ async def howsimpami(ctx):
   simppercent = random.randint(1,100)
   await ctx.send('You are '+ str(simppercent) + '% simp :smirk:')
 
-client.run(os.getenv('TOKEN'))
+@client.command()
+async def sfw(ctx):
+    url = json.loads(requests.get('https://api.waifu.pics/sfw/' + str(random.choice(topicsfw))).text)["url"]
+    embed=discord.Embed(description="here's your pic :smirk:")
+    embed.set_image(url=url)
+    await ctx.send(embed=embed)
+
+@client.command()
+async def nsfw(ctx):
+  url = json.loads(requests.get('https://api.waifu.pics/nsfw/' + str(random.choice(topicnsfw))).text)["url"]
+  embed=discord.Embed(description="We know what you are gonna do with this pic :smirk:")
+  embed.set_image(url=url)
+  if ctx.channel.is_nsfw():
+    await ctx.send(embed=embed)
+  else:
+    await ctx.send("breh this is not an nsfw channel, you smol pp fellow")
+
+client.run(os.getenv('TOKEN'))  
