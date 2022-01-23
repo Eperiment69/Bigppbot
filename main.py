fc7a7611ef3c1b101c6a45c7affbb6671498e915
Eperiment69 <72180693+Eperiment69@users.noreply.github.com>
2022-01-23 12:51:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 #import stuff
 import os
-import discord
-from discord.ext import commands
+import nextcord
+from nextcord.ext import commands
 import random
 from replit import db
 import requests
+from jikanpy import Jikan
 import json
 from links import pissedoff, waifuu, topicsfw, topicnsfw
 # import interactions
 description = "A bot, with big pp"
-intents = discord.Intents.default()
+intents = nextcord.Intents.default()
 intents.members = True
-client = commands.Bot(command_prefix='$', description=description, intents=intents)
+client = commands.Bot(command_prefix='1', description=description, intents=intents)
+jikan = Jikan()
 
 def get_quote():
   response = requests.get('https://zenquotes.io/api/random')
@@ -40,9 +42,6 @@ async def on_message(message):
           await message.channel.send(random.choice(discouragments))
       await client.process_commands(message)
 
-def update_discouragement(discouraging_message):
-  if discouragments in db.keys():
-    pass
 
 #commands
 @client.command()
@@ -68,7 +67,6 @@ async def howsimpami(ctx):
 async def howthotami(ctx):
   thotpercent = random.randint(1,100)
   await ctx.send('You are '+ str(thotpercent) + '% thot :peach:') 
-
 
 @client.command()
 async def toss(ctx):
@@ -136,14 +134,14 @@ async def dog(ctx):
 @client.command()
 async def sfw(ctx):
     url = json.loads(requests.get('https://api.waifu.pics/sfw/' + str(random.choice(topicsfw))).text)['url']
-    embed=discord.Embed(description="here's your pic :smile:")
+    embed=nextcord.Embed(description="here's your pic :smile:")
     embed.set_image(url=url)
     await ctx.send(embed=embed)
 
 @client.command()
 async def nsfw(ctx):
   url = json.loads(requests.get('https://api.waifu.pics/nsfw/' + str(random.choice(topicnsfw))).text)["url"]
-  embed=discord.Embed(description="We know what you are gonna do with this pic :smirk:")
+  embed=nextcord.Embed(description="We know what you are gonna do with this pic :smirk:")
   embed.set_image(url=url)
   if ctx.channel.is_nsfw():
     await ctx.send(embed=embed)
@@ -171,10 +169,72 @@ async def kill(ctx):
   await ctx.send(url)
 
 @client.command()
+async def anime(ctx, *anime):
+  try:
+    output = jikan.search('anime', anime)
+    embed = nextcord.Embed(title = output['results'][0]['title'], description = output['results'][0]['synopsis'])
+    embed.set_image(image = output['results'][0]['image_url'])
+    embed.add_field(name = 'Eps', value = output['results'][0]['episodes'])
+    await ctx.send(embed=embed)
+  except:
+    await ctx.send("an error occured")
+
+
+
+@client.command()
 async def gif_help(ctx):
   help = '```You can use the given gif commands:\n $nudes,\n $pissoff,\n $waifu,\n $cat,\n $dog,\n $sfw,\n $hug,\n $kick,\n $pat,\n $kill\n and a very secret command ;)```'
   await ctx.send(help)
-          
-client.run(os.getenv('TOKEN'))  
+
+@client.command()
+async def bj(ctx):
+
+  hit_number = 21
+
+  random_number = random.randint(1,10)
+  number_1 = random_number
+  number_2 = random.randint(1,10)
+  number_3 = random.randint(1,10)
+  number_4 = random.randint(1,10)
+  number_5 = random.randint(1,10)
+
+  classes = ['♣', '♥', '♠', '♦']
+  _class = random.choice(classes)
 
 
+  card_1 = str(number_1) + random.choice(classes)
+  card_2 = str(number_2) + random.choice(classes) 
+  card_3 = str(number_3) + random.choice(classes)
+  card_4 = str(number_4) + random.choice(classes)
+  card_5 = str(number_5) + random.choice(classes)
+
+  await ctx.send(card_1)
+  await ctx.send("What do you wanna do, **hit** or **stand**")
+
+  def check(m):
+   return m.author == ctx.author and m.channel == ctx.channel and \
+        m.content.lower() in ["hit", "stand"]
+
+  msg = await client.wait_for('message', check=check)
+
+  if msg.content.lower() == "hit":
+    if number_1 + number_2 < hit_number:
+      await ctx.send(card_1 + ',' +card_2)
+      await ctx.send("What do you wanna do, **hit** or **stand**")
+
+      msg_2 = await client.wait_for('message', check=check)
+
+      if msg_2.content.lower() == "hit":
+        if number_1 + number_2 + number_3 < hit_number:
+          await ctx.send(card_1 + ',' +card_2 + ',' + card_3)
+          await ctx.send("What do you wanna do, **hit** or **stand**")
+        else:
+          await ctx.send('More than 21, you lose' + ' ' + card_1 + ',' +card_2 + ',' + card_3)
+
+
+    else:
+      await ctx.send('More than 21, you lose' + ' ' + card_1 + ',' +card_2)
+  elif msg.content.lower() == "stand":
+    await ctx.send('sup')
+
+client.run(os.getenv('TOKEN'))   
